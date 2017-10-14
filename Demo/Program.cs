a3688db39abfb1cb64fa45e982475a401daa6552
Simon Pavlik <simon.pavlik@europe.com>
2017-10-14 23:17:33 +0200

    Addes som test data in CommandLineApp which will be created.

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL;
+using DAL.Entities;
+using DAL.Enumerations;
+using DAL.IdentityEntities;
 
 namespace Demo
 {
@@ -11,10 +15,48 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+
+            //Always deletes old db before creating new one
+            //Database.SetInitializer<DatabaseContext>(new DropCreateDatabaseAlways<DatabaseContext>());
+
+            Database.SetInitializer<DatabaseContext>(new DatabaseContextInitializer());
+
             using (var db = new DatabaseContext())
             {
-                var user = db.AppUsers.Find(1);
-                Console.WriteLine(user);
+                // UserName should be unique so this is just a helper.
+                var RandomNumber = new Random().Next(99999999);
+
+                AppUser newUser = new AppUser
+                {
+                    //Id = 0,
+                    AccessFailedCount = 0,
+                    //CreatedOn = DateTime.Today,
+                    Gender = Gender.Male,
+                    Name = "Name",
+                    Surname = "Surname",
+                    UserName = "User" + RandomNumber
+                };
+
+                //newUser.CreatedBy = newUser;
+                db.AppUsers.Add(newUser);
+                db.SaveChanges();
+
+                Status newStatus = new Status()
+                {
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = DateTime.Now,
+                    Private = true,
+                    Text = "You should give us 8 points for this!",
+                    CreatedBy = newUser,
+                };
+
+                db.Statuses.Add(newStatus);
+                db.SaveChanges();
+
+
+                var user = db.AppUsers.Find(newUser.Id);
+                Console.WriteLine($"Name: {user.UserName} \n {user.Statuses.First().Text}");
+                Console.ReadLine();
             }
         }
     }
