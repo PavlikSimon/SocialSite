aa85b0da9f57948ae73570d70037226e1c09bdc0
Simon Pavlik <simon.pavlik@europe.com>
2017-10-15 00:47:45 +0200

    Revert "CEOs added to initializer :)"
    This reverts commit cd9cb22b4c3f4ea3b3473b4f3b695f08f3611761.

--- DAL/DatabaseContextInitializer.cs
+++ DAL/DatabaseContextInitializer.cs
@@ -23,29 +23,6 @@ namespace DAL
             };
             context.AppUsers.Add(newUser);
             context.SaveChanges();
-
-            AppUser simon = new AppUser
-            {
-                AccessFailedCount = 0,
-                Gender = Gender.Male,
-                Name = "Simon",
-                Surname = "Pavlik",
-                UserName = "CEO-Simon"
-            };
-            context.AppUsers.Add(simon);
-            context.SaveChanges();
-
-            AppUser honza = new AppUser
-            {
-                AccessFailedCount = 0,
-                Gender = Gender.Male,
-                Name = "Honza",
-                Surname = "Cech",
-                UserName = "CEO-Honza"
-            };
-            context.AppUsers.Add(honza);
-            context.SaveChanges();
-
             base.Seed(context);
         }
     }
