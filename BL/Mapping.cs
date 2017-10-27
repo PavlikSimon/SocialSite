6a8abb33c4d70c6c187e615c4d29a9729d25bcba
Simon Pavlik <simon.pavlik@europe.com>
2017-10-28 01:49:48 +0200

    Mappings added

---
+++ BL/Mapping.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BL.DTO;
+using DAL.Entities;
+using DAL.IdentityEntities;
+
+namespace BL
+{
+    public static class Mapping
+    {
+        public static IMapper Mapper { get; }
+        static Mapping()
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                
+                c.CreateMap<Comment, CommentDTO>().ReverseMap();
+                c.CreateMap<Event, EventDTO>().ReverseMap();
+                c.CreateMap<Group, GroupDTO>().ReverseMap();
+                c.CreateMap<Message, MessageDTO>().ReverseMap();
+                c.CreateMap<AppUser, AppUserDTO>().ReverseMap();
+            });
+            Mapper = config.CreateMapper();
+        }
+    }
+}
