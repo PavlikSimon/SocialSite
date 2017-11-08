ec61b94c22bdb74fe6ec8fac109d8f2afb522312
Simon Pavlik <simon.pavlik@europe.com>
2017-11-08 16:59:44 +0100

    FacadeBase created

---
+++ BussinessLayer/Facades/Common/FacadeBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Riganti.Utils.Infrastructure.Core;
+
+
+namespace BussinessLayer.Facades.Common
+{
+    public abstract class FacadeBase
+    {
+        protected readonly IUnitOfWorkProvider UnitOfWorkProvider;
+        protected FacadeBase(IUnitOfWorkProvider unitOfWorkProvider)
+        {
+            UnitOfWorkProvider = unitOfWorkProvider;
+        }
+
+    }
+}
