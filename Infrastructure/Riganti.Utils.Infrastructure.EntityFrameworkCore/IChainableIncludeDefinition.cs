6e7b3b9a5c6354a70d3c559b6f78e9eeb58d4378
Simon Pavlik <simon.pavlik@europe.com>
2017-11-08 17:48:06 +0100

    Riganti.Inf***.EFCore removed

--- Infrastructure/Riganti.Utils.Infrastructure.EntityFrameworkCore/IChainableIncludeDefinition.cs
+++
@@ -1,9 +0,0 @@
-﻿using Riganti.Utils.Infrastructure.Core;
-
-namespace Riganti.Utils.Infrastructure.EntityFrameworkCore
-{
-    public interface IChainableIncludeDefinition<TEntity, TResult>
-        where TEntity : class where TResult : class
-    {
-    }
-}
