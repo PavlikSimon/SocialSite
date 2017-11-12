2de9e58f9a61d6209af073583c6db3d526790884
Simon Pavlik <simon.pavlik@europe.com>
2017-11-12 22:02:03 +0100

    MessageFacade CRUD deleted

--- BussinessLayer/Facades/MessageFacade.cs
+++ BussinessLayer/Facades/MessageFacade.cs
@@ -25,6 +25,43 @@ namespace BussinessLayer.Facades
             this.messageService = messageService;
         }
 
- 
+
+
+        public int Create(MessageDTO entityDto)
+        {
+            int returnValue;
+            using (UnitOfWorkProvider.Create())
+            {
+                returnValue = messageService.Create(entityDto);
+            }
+            return returnValue;
+        }
+
+        public void Update(MessageDTO entityDto)
+        {
+            using (UnitOfWorkProvider.Create())
+            {
+                messageService.Update(entityDto);
+            }
+        }
+
+        public void Delete(MessageDTO entityDto)
+        {
+            using (UnitOfWorkProvider.Create())
+            {
+                messageService.Delete(entityDto.Id);
+            }
+        }
+
+        public async Task<MessageDTO> GetByIdAsync(int entityId)
+        {
+            return await messageService.GetAsync(entityId);
+        }
+
+        public IEnumerable<MessageDTO> ListAll()
+        {
+            return messageService.ListAllAsync().Result.Items;
+        }
+
     }
 }
