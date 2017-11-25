bf47c7355213475a129a4c5fd8b21d11753827be
Simon Pavlik <simon.pavlik@europe.com>
2017-11-26 00:33:56 +0100

    Gender namespace adjusted to BL, was DAL

--- BussinessLayer/DTO/AppUserDTO.cs
+++ BussinessLayer/DTO/AppUserDTO.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BussinessLayer.DataTransferObjects.Common;
-using DAL.Enumerations;
+using BussinessLayer.DTO.Enumerations;
 
 namespace BussinessLayer.DTO
 {
